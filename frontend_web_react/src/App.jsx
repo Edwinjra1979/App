950a5243f8199a93ad65f3b6c79f1e7883dc92f3
Edwinjra1979 <edwinjra@gmail.com>
2024-01-08 18:57:48 -0500

    Rutas

--- frontend_web_react/src/App.jsx
+++ frontend_web_react/src/App.jsx
@@ -1,6 +1,18 @@
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import { TasksPage } from './pages/TasksPage';
+import { TaskFormPage } from './pages/TaskFormPage';
+
+
 function App() {
   return (
-    <div>Hello World</div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Navigate to='/tasks'></Navigate>}></Route>
+        <Route path="/tasks" element={<TasksPage></TasksPage>}></Route>
+        <Route path="/tasks-create" element={<TaskFormPage></TaskFormPage>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
