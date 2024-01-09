81ead49d59aa75d55da490a785d130d3306ba9b9
Edwinjra1979 <edwinjra@gmail.com>
2024-01-08 23:48:40 -0500

    Crud

--- frontend_web_react/src/App.jsx
+++ frontend_web_react/src/App.jsx
@@ -1,17 +1,24 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import { TasksPage } from './pages/TasksPage';
 import { TaskFormPage } from './pages/TaskFormPage';
+import { Navigation } from './components/Navigation';
+import { Toaster } from "react-hot-toast";
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter> 
+    <div className="container mx-auto">
+    <Navigation />
       <Routes>
-
         <Route path="/" element={<Navigate to='/tasks'></Navigate>}></Route>
         <Route path="/tasks" element={<TasksPage></TasksPage>}></Route>
         <Route path="/tasks-create" element={<TaskFormPage></TaskFormPage>}></Route>
+        <Route path="/tasks/:id" element={<TaskFormPage></TaskFormPage>}></Route>
       </Routes>
+      <Toaster />
+    </div>    
+    
     </BrowserRouter>
   )
 }
---
+++ frontend_web_react/src/components/TasksList.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { getAllTasks } from "../api/task.api";
+import { TaskCard } from "./TaskCard";
+
+export function TasksList() {
+
+    const [tasks, setTasks] = useState([])
+
+    useEffect(()=> {
+        async function loadTask(){
+            const res = await getAllTasks()
+            setTasks(res.data)
+        }
+        loadTask();
+    }, [])
+  return (
+    <div className="grid grid-cols-3 gap-3">
+        {tasks.map(task => (
+            <TaskCard key={task.id} task={task}/>
+        ))}
+    </div>
+  )
+}
